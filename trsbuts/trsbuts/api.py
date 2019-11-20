e6a43840429d5846b12a4fd3498e6128f65516f3
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-20 13:28:32 +0300

    initiation

---
+++ trsbuts/trsbuts/api.py
@@ -0,0 +1,35 @@
+import frappe
+import requests
+import json
+
+
+@frappe.whitelist()
+def test_integration(test, testtoken):
+    servicepath = "/UTS/rest/kurum/firmaSorgula"
+    # Replace with the correct URL
+    url = ""
+    if test == "real":
+        url = frappe.db.get_single_value("TR UTS Integration Settings", "server")
+    if test == "test":
+        url = frappe.db.get_single_value("TR UTS Integration Settings", "testserver")
+    # her web servis çağrısının başlık (header) kısmına utsToken etiketiyle sistem token’ının değerini eklemelidir
+    company = frappe.defaults.get_user_default("Company")
+    __headers = {
+        'utsToken': testtoken,
+        'Content-Type': frappe.db.get_single_value("TR UTS Integration Settings", "contenttype")
+    }
+
+    servicedata = "{"
+    servicedata = servicedata + "\"VRG\":\"" + frappe.db.get_value("Company", company, "tax_id") + "\""
+    servicedata = servicedata + "}"
+
+    servicerequestdatafields = servicedata
+
+    _requesturl = url + servicepath
+
+    s = requests.Session()
+    s.headers.update(__headers)
+    # Web servislerin tamamında HTTP request method olarak “POST” metodu kullanılmaktadır.
+    response = s.post(_requesturl, servicerequestdatafields)
+
+    return response.text
--- trsbuts/trsbuts/doctype/tr_uts_company_settings/tr_uts_company_settings.js
+++ trsbuts/trsbuts/doctype/tr_uts_company_settings/tr_uts_company_settings.js
@@ -5,4 +5,32 @@ frappe.ui.form.on('TR UTS Company Settings', {
 	// refresh: function(frm) {
 
 	// }
+	check_integration: function(frm){
+	    if(frm.doc.systemtoken!=""){
+	        frappe.call({
+	            method: "trsbuts.api.test_integration",
+	            args:{
+	                test: "real",
+                    testtoken: frm.doc.systemtoken
+	            },
+	            callback: function(r){
+                    frm.set_value("result", r.message)
+	            }
+	        })
+	    }
+	},
+		check_testsystemintegration: function(frm){
+	    if(frm.doc.testsystemtoken!=""){
+	        frappe.call({
+	            method: "trsbuts.api.test_integration",
+	            args:{
+	                test: "test",
+                    testtoken: frm.doc.testsystemtoken
+	            },
+	            callback: function(r){
+                    frm.set_value("testsystemresult", r.message)
+	            }
+	        })
+	    }
+	}
 });
