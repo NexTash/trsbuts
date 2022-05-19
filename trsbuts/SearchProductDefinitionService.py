7dd4119f32f0f80b69022254b95deda278058227
framras <tufan.kaynak@framras.com>
2022-05-19 21:39:55 +0300

    Denormalizing Notes as Child Table

--- trsbuts/SearchProductDefinitionService.py
+++ trsbuts/SearchProductDefinitionService.py
@@ -3,18 +3,21 @@ import trsbuts.connectors.UTSConnection as UTSConnection
 
 class SearchProductDefinitionService:
     def __index__(self):
-        self._utsconnection = UTSConnection()
-        self._servicepath = "/UTS/rest/tibbiCihaz/urunSorgula"
+        self._servicepath = "/UTS/rest/tibbiCihaz"
 
     # ÜRÜN SORGULAMA SERVİSİ
     # Birincil ürün numarası ve üretici ÜTS firma numarası ile ürün bilgilerini sorgulamasını sağlayan servistir.
     def urunsorgula(self, uno: str):
-        servicepath = self._servicepath + "/urunSorgula"
         servicedata = "{"
-        servicedata = servicedata + "\"UNO\":\"" + uno + "\""
-        servicedata = servicedata + "}"
-
-        return connect(servicepath, servicedata)
+        if uno != "":
+            servicedata = servicedata + "\"UNO\":\"" + uno + "\""
+            c: UTSConnection = UTSConnection()
+            return c.connect(
+                self._servicepath + "/urunSorgula",
+                servicedata + "}"
+            )
+        else:
+            return ""
 
     # BÜTÜN ÜRÜNLERİ SORGULAMA SERVİSİ
     # Firmaların kendi ürünlerini çoklu olarak sorgulamalarını sağlayan servistir.
