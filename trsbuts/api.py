c7a0759de3dbf2bdb6f7d8e46be5de673c523c5e
framras <tufan.kaynak@framras.com>
2022-05-21 23:14:02 +0300

    Denormalizing Notes as Child Table

--- trsbuts/api.py
+++ trsbuts/api.py
@@ -107,7 +107,7 @@ def get_tekilurun_by_batch(batch):
     object_name = "Tekil Ürün"
     q = InquiringService()
     d: dict = q.tekilurunsorgula(uno=l[0].get('barcode'), lno=str.strip(b.vendor_batch))
-    individual: dict = d.get("SNC")
+    individual: dict = d.get("SNC")[0]
     if len(individual) == 0:
         return ""
     for children in frappe.get_all(
