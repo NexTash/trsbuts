80611613a9fd1acb0b4c2e1882ab64ca4842519c
framras <tufan.kaynak@framras.com>
2022-05-29 20:56:28 +0300

    Building ÜTS services

--- trsbuts/api.py
+++ trsbuts/api.py
@@ -128,13 +128,13 @@ def refresh_all_items():
                             'item': i.name
                         },
                         fields={
-                            "batch",
+                            "name",
                             "vendor_batch"
                         }):
-                    get_tekilurun_by_batch(batch.get("batch"), batch.get("vendor_batch"))
-                    get_bildirim_by_batch((batch.get("batch"), batch.get("vendor_batch")))
-                    get_askidakitekilurun_by_batch(batch.get("batch"), batch.get("vendor_batch"))
-                    get_sistemdisitekilurun_by_batch(batch.get("batch"), batch.get("vendor_batch"))
+                    get_tekilurun_by_batch(batch.get("name"), batch.get("vendor_batch"))
+                    get_bildirim_by_batch((batch.get("name"), batch.get("vendor_batch")))
+                    get_askidakitekilurun_by_batch(batch.get("name"), batch.get("vendor_batch"))
+                    get_sistemdisitekilurun_by_batch(batch.get("name"), batch.get("vendor_batch"))
 
 
 @frappe.whitelist()
